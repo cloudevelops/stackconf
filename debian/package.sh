28f44284fadd4a0f1a654d112ec909f31c2f7577
Pavel Fesenko <pavel.fesenko@cloudevelops.com>
2021-07-19 21:03:23 +0200

    update version to 0.1.25

--- debian/package.sh
+++ debian/package.sh
@@ -2,4 +2,4 @@
 rm -rf /tmp/stackconf
 mkdir -p /tmp/stackconf/usr/local/bin
 cp -a $GOPATH/bin/stackconf /tmp/stackconf/usr/local/bin
-fpm -s dir -t deb -C /tmp/stackconf --name stackconf --version 0.1.24 --description "stack orchestration engine" --package /tmp/stackconf
+fpm -s dir -t deb -C /tmp/stackconf --name stackconf --version 0.1.25 --description "stack orchestration engine" --package /tmp/stackconf
