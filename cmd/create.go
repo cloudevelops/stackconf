4f2167823bc29e0c89f45f56a63ba8428d0bea24
Ondřej Lachman <ondrej.lachman@cloudevelops.com>
2023-03-26 11:38:27 +0200

    release 0.1.27

--- cmd/create.go
+++ cmd/create.go
@@ -66,7 +66,7 @@ var createCmd = &cobra.Command{
 	Short: "Create a new stackconf host",
 	Long:  `Create a new stackconf host.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Debugf("Create command: starting, version 0.1.26")
+		log.Debugf("Create command: starting, version 0.1.27")
 		if noop {
 			log.Debugf("NOOP ENABLED! This create run will not do any changes.")
 		}
